74c68cc6f64dc2eedc70e06d70a4243754bd383a
Lolo280374 <56828143+Lolo280374@users.noreply.github.com>
2026-02-19 22:04:36 +0100

    finished all the backend stuff I wanted to do and tailored the UI

--- script.js
+++ script.js
@@ -2,261 +2,26 @@ const githubBtn = document.getElementById('github');
 const githubLink = "https://github.com/Lolo280374/fuckCustoms";
 const flavortownBtn = document.getElementById('flavortown');
 const flavortownLink = "https://flavortown.hackclub.com/projects/13644";
-const originCountryEl = document.getElementById('origin-co');
 const destinationCountryEl = document.getElementById('dest-co');
+let countryRates = {};
+let exchangeRates = {};
+let countries = [];
+let destChoice;
 
-const countries = [ 
-  {label: 'Afghanistan', value: 'AF'}, 
-  {label: 'Åland Islands', value: 'AX'}, 
-  {label: 'Albania', value: 'AL'}, 
-  {label: 'Algeria', value: 'DZ'}, 
-  {label: 'American Samoa', value: 'AS'}, 
-  {label: 'Andorra', value: 'AD'}, 
-  {label: 'Angola', value: 'AO'}, 
-  {label: 'Anguilla', value: 'AI'}, 
-  {label: 'Antarctica', value: 'AQ'}, 
-  {label: 'Antigua and Barbuda', value: 'AG'}, 
-  {label: 'Argentina', value: 'AR'}, 
-  {label: 'Armenia', value: 'AM'}, 
-  {label: 'Aruba', value: 'AW'}, 
-  {label: 'Australia', value: 'AU'}, 
-  {label: 'Austria', value: 'AT'}, 
-  {label: 'Azerbaijan', value: 'AZ'}, 
-  {label: 'Bahamas', value: 'BS'}, 
-  {label: 'Bahrain', value: 'BH'}, 
-  {label: 'Bangladesh', value: 'BD'}, 
-  {label: 'Barbados', value: 'BB'}, 
-  {label: 'Belarus', value: 'BY'}, 
-  {label: 'Belgium', value: 'BE'}, 
-  {label: 'Belize', value: 'BZ'}, 
-  {label: 'Benin', value: 'BJ'}, 
-  {label: 'Bermuda', value: 'BM'}, 
-  {label: 'Bhutan', value: 'BT'}, 
-  {label: 'Bolivia', value: 'BO'}, 
-  {label: 'Bosnia and Herzegovina', value: 'BA'}, 
-  {label: 'Botswana', value: 'BW'}, 
-  {label: 'Bouvet Island', value: 'BV'}, 
-  {label: 'Brazil', value: 'BR'}, 
-  {label: 'British Indian Ocean Territory', value: 'IO'}, 
-  {label: 'Brunei Darussalam', value: 'BN'}, 
-  {label: 'Bulgaria', value: 'BG'}, 
-  {label: 'Burkina Faso', value: 'BF'}, 
-  {label: 'Burundi', value: 'BI'}, 
-  {label: 'Cambodia', value: 'KH'}, 
-  {label: 'Cameroon', value: 'CM'}, 
-  {label: 'Canada', value: 'CA'}, 
-  {label: 'Cape Verde', value: 'CV'}, 
-  {label: 'Cayman Islands', value: 'KY'}, 
-  {label: 'Central African Republic', value: 'CF'}, 
-  {label: 'Chad', value: 'TD'}, 
-  {label: 'Chile', value: 'CL'}, 
-  {label: 'China', value: 'CN'}, 
-  {label: 'Christmas Island', value: 'CX'}, 
-  {label: 'Cocos (Keeling) Islands', value: 'CC'}, 
-  {label: 'Colombia', value: 'CO'}, 
-  {label: 'Comoros', value: 'KM'}, 
-  {label: 'Congo', value: 'CG'}, 
-  {label: 'Congo, The Democratic Republic of the', value: 'CD'}, 
-  {label: 'Cook Islands', value: 'CK'}, 
-  {label: 'Costa Rica', value: 'CR'}, 
-  {label: 'Cote d\'Ivoire', value: 'CI'}, 
-  {label: 'Croatia', value: 'HR'}, 
-  {label: 'Cuba', value: 'CU'}, 
-  {label: 'Cyprus', value: 'CY'}, 
-  {label: 'Czech Republic', value: 'CZ'}, 
-  {label: 'Denmark', value: 'DK'}, 
-  {label: 'Djibouti', value: 'DJ'}, 
-  {label: 'Dominica', value: 'DM'}, 
-  {label: 'Dominican Republic', value: 'DO'}, 
-  {label: 'Ecuador', value: 'EC'}, 
-  {label: 'Egypt', value: 'EG'}, 
-  {label: 'El Salvador', value: 'SV'}, 
-  {label: 'Equatorial Guinea', value: 'GQ'}, 
-  {label: 'Eritrea', value: 'ER'}, 
-  {label: 'Estonia', value: 'EE'}, 
-  {label: 'Ethiopia', value: 'ET'}, 
-  {label: 'Falkland Islands (Malvinas)', value: 'FK'}, 
-  {label: 'Faroe Islands', value: 'FO'}, 
-  {label: 'Fiji', value: 'FJ'}, 
-  {label: 'Finland', value: 'FI'}, 
-  {label: 'France', value: 'FR'}, 
-  {label: 'French Guiana', value: 'GF'}, 
-  {label: 'French Polynesia', value: 'PF'}, 
-  {label: 'French Southern Territories', value: 'TF'}, 
-  {label: 'Gabon', value: 'GA'}, 
-  {label: 'Gambia', value: 'GM'}, 
-  {label: 'Georgia', value: 'GE'}, 
-  {label: 'Germany', value: 'DE'}, 
-  {label: 'Ghana', value: 'GH'}, 
-  {label: 'Gibraltar', value: 'GI'}, 
-  {label: 'Greece', value: 'GR'}, 
-  {label: 'Greenland', value: 'GL'}, 
-  {label: 'Grenada', value: 'GD'}, 
-  {label: 'Guadeloupe', value: 'GP'}, 
-  {label: 'Guam', value: 'GU'}, 
-  {label: 'Guatemala', value: 'GT'}, 
-  {label: 'Guernsey', value: 'GG'}, 
-  {label: 'Guinea', value: 'GN'}, 
-  {label: 'Guinea-Bissau', value: 'GW'}, 
-  {label: 'Guyana', value: 'GY'}, 
-  {label: 'Haiti', value: 'HT'}, 
-  {label: 'Heard Island and Mcdonald Islands', value: 'HM'}, 
-  {label: 'Holy See (Vatican City State)', value: 'VA'}, 
-  {label: 'Honduras', value: 'HN'}, 
-  {label: 'Hong Kong', value: 'HK'}, 
-  {label: 'Hungary', value: 'HU'}, 
-  {label: 'Iceland', value: 'IS'}, 
-  {label: 'India', value: 'IN'}, 
-  {label: 'Indonesia', value: 'ID'}, 
-  {label: 'Iran, Islamic Republic Of', value: 'IR'}, 
-  {label: 'Iraq', value: 'IQ'}, 
-  {label: 'Ireland', value: 'IE'}, 
-  {label: 'Isle of Man', value: 'IM'}, 
-  {label: 'Israel', value: 'IL'}, 
-  {label: 'Italy', value: 'IT'}, 
-  {label: 'Jamaica', value: 'JM'}, 
-  {label: 'Japan', value: 'JP'}, 
-  {label: 'Jersey', value: 'JE'}, 
-  {label: 'Jordan', value: 'JO'}, 
-  {label: 'Kazakhstan', value: 'KZ'}, 
-  {label: 'Kenya', value: 'KE'}, 
-  {label: 'Kiribati', value: 'KI'}, 
-  {label: 'Korea, Democratic People\'s Republic of', value: 'KP'}, 
-  {label: 'Korea, Republic of', value: 'KR'}, 
-  {label: 'Kuwait', value: 'KW'}, 
-  {label: 'Kyrgyzstan', value: 'KG'}, 
-  {label: 'Lao People\'S Democratic Republic', value: 'LA'}, 
-  {label: 'Latvia', value: 'LV'}, 
-  {label: 'Lebanon', value: 'LB'}, 
-  {label: 'Lesotho', value: 'LS'}, 
-  {label: 'Liberia', value: 'LR'}, 
-  {label: 'Libyan Arab Jamahiriya', value: 'LY'}, 
-  {label: 'Liechtenstein', value: 'LI'}, 
-  {label: 'Lithuania', value: 'LT'}, 
-  {label: 'Luxembourg', value: 'LU'}, 
-  {label: 'Macao', value: 'MO'}, 
-  {label: 'Macedonia, The Former Yugoslav Republic of', value: 'MK'}, 
-  {label: 'Madagascar', value: 'MG'}, 
-  {label: 'Malawi', value: 'MW'}, 
-  {label: 'Malaysia', value: 'MY'}, 
-  {label: 'Maldives', value: 'MV'}, 
-  {label: 'Mali', value: 'ML'}, 
-  {label: 'Malta', value: 'MT'}, 
-  {label: 'Marshall Islands', value: 'MH'}, 
-  {label: 'Martinique', value: 'MQ'}, 
-  {label: 'Mauritania', value: 'MR'}, 
-  {label: 'Mauritius', value: 'MU'}, 
-  {label: 'Mayotte', value: 'YT'}, 
-  {label: 'Mexico', value: 'MX'}, 
-  {label: 'Micronesia, Federated States of', value: 'FM'}, 
-  {label: 'Moldova, Republic of', value: 'MD'}, 
-  {label: 'Monaco', value: 'MC'}, 
-  {label: 'Mongolia', value: 'MN'}, 
-  {label: 'Montserrat', value: 'MS'}, 
-  {label: 'Morocco', value: 'MA'}, 
-  {label: 'Mozambique', value: 'MZ'}, 
-  {label: 'Myanmar', value: 'MM'}, 
-  {label: 'Namibia', value: 'NA'}, 
-  {label: 'Nauru', value: 'NR'}, 
-  {label: 'Nepal', value: 'NP'}, 
-  {label: 'Netherlands', value: 'NL'}, 
-  {label: 'Netherlands Antilles', value: 'AN'}, 
-  {label: 'New Caledonia', value: 'NC'}, 
-  {label: 'New Zealand', value: 'NZ'}, 
-  {label: 'Nicaragua', value: 'NI'}, 
-  {label: 'Niger', value: 'NE'}, 
-  {label: 'Nigeria', value: 'NG'}, 
-  {label: 'Niue', value: 'NU'}, 
-  {label: 'Norfolk Island', value: 'NF'}, 
-  {label: 'Northern Mariana Islands', value: 'MP'}, 
-  {label: 'Norway', value: 'NO'}, 
-  {label: 'Oman', value: 'OM'}, 
-  {label: 'Pakistan', value: 'PK'}, 
-  {label: 'Palau', value: 'PW'}, 
-  {label: 'Palestinian Territory, Occupied', value: 'PS'}, 
-  {label: 'Panama', value: 'PA'}, 
-  {label: 'Papua New Guinea', value: 'PG'}, 
-  {label: 'Paraguay', value: 'PY'}, 
-  {label: 'Peru', value: 'PE'}, 
-  {label: 'Philippines', value: 'PH'}, 
-  {label: 'Pitcairn', value: 'PN'}, 
-  {label: 'Poland', value: 'PL'}, 
-  {label: 'Portugal', value: 'PT'}, 
-  {label: 'Puerto Rico', value: 'PR'}, 
-  {label: 'Qatar', value: 'QA'}, 
-  {label: 'Reunion', value: 'RE'}, 
-  {label: 'Romania', value: 'RO'}, 
-  {label: 'Russian Federation', value: 'RU'}, 
-  {label: 'RWANDA', value: 'RW'}, 
-  {label: 'Saint Helena', value: 'SH'}, 
-  {label: 'Saint Kitts and Nevis', value: 'KN'}, 
-  {label: 'Saint Lucia', value: 'LC'}, 
-  {label: 'Saint Pierre and Miquelon', value: 'PM'}, 
-  {label: 'Saint Vincent and the Grenadines', value: 'VC'}, 
-  {label: 'Samoa', value: 'WS'}, 
-  {label: 'San Marino', value: 'SM'}, 
-  {label: 'Sao Tome and Principe', value: 'ST'}, 
-  {label: 'Saudi Arabia', value: 'SA'}, 
-  {label: 'Senegal', value: 'SN'}, 
-  {label: 'Serbia and Montenegro', value: 'CS'}, 
-  {label: 'Seychelles', value: 'SC'}, 
-  {label: 'Sierra Leone', value: 'SL'}, 
-  {label: 'Singapore', value: 'SG'}, 
-  {label: 'Slovakia', value: 'SK'}, 
-  {label: 'Slovenia', value: 'SI'}, 
-  {label: 'Solomon Islands', value: 'SB'}, 
-  {label: 'Somalia', value: 'SO'}, 
-  {label: 'South Africa', value: 'ZA'}, 
-  {label: 'South Georgia and the South Sandwich Islands', value: 'GS'}, 
-  {label: 'Spain', value: 'ES'}, 
-  {label: 'Sri Lanka', value: 'LK'}, 
-  {label: 'Sudan', value: 'SD'}, 
-  {label: 'Surilabel', value: 'SR'}, 
-  {label: 'Svalbard and Jan Mayen', value: 'SJ'}, 
-  {label: 'Swaziland', value: 'SZ'}, 
-  {label: 'Sweden', value: 'SE'}, 
-  {label: 'Switzerland', value: 'CH'}, 
-  {label: 'Syrian Arab Republic', value: 'SY'}, 
-  {label: 'Taiwan, Province of China', value: 'TW'}, 
-  {label: 'Tajikistan', value: 'TJ'}, 
-  {label: 'Tanzania, United Republic of', value: 'TZ'}, 
-  {label: 'Thailand', value: 'TH'}, 
-  {label: 'Timor-Leste', value: 'TL'}, 
-  {label: 'Togo', value: 'TG'}, 
-  {label: 'Tokelau', value: 'TK'}, 
-  {label: 'Tonga', value: 'TO'}, 
-  {label: 'Trinidad and Tobago', value: 'TT'}, 
-  {label: 'Tunisia', value: 'TN'}, 
-  {label: 'Turkey', value: 'TR'}, 
-  {label: 'Turkmenistan', value: 'TM'}, 
-  {label: 'Turks and Caicos Islands', value: 'TC'}, 
-  {label: 'Tuvalu', value: 'TV'}, 
-  {label: 'Uganda', value: 'UG'}, 
-  {label: 'Ukraine', value: 'UA'}, 
-  {label: 'United Arab Emirates', value: 'AE'}, 
-  {label: 'United Kingdom', value: 'GB'}, 
-  {label: 'United States', value: 'US'}, 
-  {label: 'United States Minor Outlying Islands', value: 'UM'}, 
-  {label: 'Uruguay', value: 'UY'}, 
-  {label: 'Uzbekistan', value: 'UZ'}, 
-  {label: 'Vanuatu', value: 'VU'}, 
-  {label: 'Venezuela', value: 'VE'}, 
-  {label: 'Viet Nam', value: 'VN'}, 
-  {label: 'Virgin Islands, British', value: 'VG'}, 
-  {label: 'Virgin Islands, U.S.', value: 'VI'}, 
-  {label: 'Wallis and Futuna', value: 'WF'}, 
-  {label: 'Western Sahara', value: 'EH'}, 
-  {label: 'Yemen', value: 'YE'}, 
-  {label: 'Zambia', value: 'ZM'}, 
-  {label: 'Zimbabwe', value: 'ZW'} 
-];
+const valueInput = document.getElementById('value-ship');
+const estValueDisplay = document.getElementById('estValue');
+const currencyDisplay = document.querySelector('.estCurrency');
+const superscriptThree = document.querySelector('.estCurrency #dutyDisclaimer');
+const bottomDutyDisclaimer = document.querySelector('.priceDisclaimers #dutyDisclaimer');
+const noDataDisclaimer = document.getElementById('noDataDisclaimer');
 
+valueInput.addEventListener('input', calculateEstimate);
 lucide.createIcons();
 
 if (githubBtn){
     githubBtn.addEventListener('click', (e) => {
         githubBtn.blur();
-        window.open(githubLink, "_blank", "noopener");
+        window.open(githubLink, '_blank', "noopener");
     });
 }
 
@@ -267,17 +32,69 @@ if (flavortownBtn){
     });
 }
 
-const originList = countries.filter(c => c.value === 'US' || c.value === 'GB');
-const originChoice = new Choices(originCountryEl, {
-    choices: originList,
-    searchEnabled: false,
-    itemSelectText: '',
-    shouldSort: false
-});
+Promise.all([
+    fetch('countries.json').then(res => res.json()),
+    fetch('countryRates.json').then(res => res.json()),
+    fetch('https://open.er-api.com/v6/latest/USD').then(res => res.json())
+])
+.then(([fetchedCountries, fetchedRates, fetchedExchange]) => {
+    countries = fetchedCountries;
+    countryRates = fetchedRates;
+    exchangeRates = fetchedExchange.rates;
 
-const destChoice = new Choices(destinationCountryEl, {
-    choices: countries,
-    searchEnabled: true,
-    itemSelectText: '',
-    shouldSort: false
-});
+    destChoice = new Choices(destinationCountryEl, {
+        choices: countries,
+        searchEnabled: true,
+        itemSelectText: '',
+        shouldSort: false
+    });
+    
+    destChoice.passedElement.element.addEventListener('change', calculateEstimate);
+    calculateEstimate();
+})
+
+function calculateEstimate(){
+    const value = parseFloat(valueInput.value) || 0;
+    const destCode = destChoice.getValue(true);
+    const rules = countryRates[destCode];
+
+    if (!rules){
+        estValueDisplay.innerText = "N/A";
+        currencyDisplay.firstChild.textContent = "???";
+        if (superscriptThree) superscriptThree.style.display = '';
+        if (bottomDutyDisclaimer) bottomDutyDisclaimer.style.display = 'none';
+        if (noDataDisclaimer) noDataDisclaimer.style.display = '';
+        return;
+    }
+
+    if (noDataDisclaimer) noDataDisclaimer.style.display = 'none';
+    let calculatedVAT = 0;
+    let calculatedDutyFees = 0;
+    let appliedHandlingFees = 0;
+
+    if (value > rules.vatThreshold){
+        calculatedVAT = value * rules.vatRate;
+        appliedHandlingFees = rules.handlingFee;
+    }
+
+    if (value > rules.dutyThreshold && rules.avgDutyRate > 0){
+        calculatedDutyFees = value * rules.avgDutyRate;
+        if (superscriptThree) superscriptThree.style.display = '';
+        if (bottomDutyDisclaimer) bottomDutyDisclaimer.style.display = '';
+    } else {
+        if (superscriptThree) superscriptThree.style.display = 'none';
+        if (bottomDutyDisclaimer) bottomDutyDisclaimer.style.display = 'none';
+    }
+
+    const totalUSD = calculatedVAT + calculatedDutyFees + appliedHandlingFees;
+    const conversionRate = exchangeRates[rules.currency] || 1;
+    const totalConverted = totalUSD * conversionRate;
+
+    if (totalConverted === 0 && value > 0){
+        estValueDisplay.innerText = "0.00";
+        currencyDisplay.firstChild.textContent = rules.currency + " (no tax!) ";
+    } else{
+        estValueDisplay.innerText = totalConverted.toFixed(2);
+        currencyDisplay.firstChild.textContent = rules.currency;
+    }
+}
